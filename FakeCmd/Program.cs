4ced4bf3428fdfd84d420253103f293a3afee95c
yishundaodi <66081811+yishundaodi@users.noreply.github.com>
2020-05-29 20:04:07 -0500

    Added new features
    CHANGE LOG~~
    1. cd has many redundant commands if scammers like to use different ones (e.g. cd, cd.., cd .)
    2. tree now features an ASCII tree.
    3. help now lists various things we want scammers to do.
    4. Added systeminfo command. Scans the scammers hardware, saying that local PC is good to go, scammer PC will die in 4 days. Also utilizes "reverse shell" to "connect" to scammers PC.
    5. cd attempts to open the optical drive. 💿
    6. hack uses the MS17-010 EternalBlue CVE2017-0144 to "hack" into the scammer's PC.
    7. run tells the scammer he can run but he can't hide!
    8. Added some Easter Eggs. Find them all. ;)

--- FakeCmd/Program.cs
+++ FakeCmd/Program.cs
@@ -15,92 +15,113 @@ using System.Threading;
 
 //Official Repository: https://github.com/Dr-lecter/
 
-namespace FakeCmd
+//Branch edition by Activate Me/yishundaodi
+
+namespace C:\WINDOWS\system32\cmd.exe
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Microsoft Windows [Version 10.0.18363.815]");
+            Console.WriteLine("Microsoft Windows [Version 10.0.80081.35]");
             Console.WriteLine("(c) 2019 Microsoft Corporation. All Rights Reserved");
             
             Start:
-            Console.Write("C:\\");
+            Console.Write("C:\WINDOWS\bin:~#");
 
             var userInput = Console.ReadLine();
 
             switch (userInput)
             {
                 case "netstat":
-                Console.WriteLine("Network Connected, Detecting Foreign IP's...");
-                Thread.Sleep(2000);
-                Console.WriteLine("Please Wait...");
-                Thread.Sleep(1000);
-                Console.WriteLine("Network Scan Complete");
-                Console.WriteLine("Foreign Hackers Found: 0");             
-                Console.WriteLine("Network Infections Found: 0");
-                Console.WriteLine("Foreign IP's Found: 0");
-                Console.WriteLine("Critical System Infrastructure Status: Good");
-                Console.WriteLine("Indian Tech Support Scammers: 1");
-                goto Start;
+                 Console.WriteLine("Network Connected, Detecting Foreign IP's...");
+                 Thread.Sleep(2000);
+                 Console.WriteLine("Please Wait...");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Network Scan Complete");
+                 Console.WriteLine("Foreign Hackers Found: 0");             
+                 Console.WriteLine("Network Infections Found: 0");
+                 Console.WriteLine("Foreign IP's Found: 0");
+                 Console.WriteLine("Critical System Infrastructure Status: Good");
+                 Console.WriteLine("Indian Tech Support Scammers: 1");
+                 goto Start;
                     
                 case "ipconfig":
-                Console.WriteLine("Network Connected, Detecting Foreign IP's...");
-                Thread.Sleep(2000);
-                Console.WriteLine("Please Wait...");
-                Thread.Sleep(1000);
-                Console.WriteLine("Network Scan Complete");
-                Console.WriteLine("Foreign Hackers Found: 0");             
-                Console.WriteLine("Network Infections Found: 0");
-                Console.WriteLine("Foreign IP's Found: 0");
-                Console.WriteLine("Critical System Infrastructure Status: Good");
-                Console.WriteLine("Indian Tech Support Scammers: 1");
-                goto Start;
+                 Console.WriteLine("Network Connected, Detecting Foreign IP's...");
+                 Thread.Sleep(2000);
+                 Console.WriteLine("Please Wait...");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Network Scan Complete");
+                 Console.WriteLine("Foreign Hackers Found: 0");             
+                 Console.WriteLine("Network Infections Found: 0");
+                 Console.WriteLine("Foreign IP's Found: 0");
+                 Console.WriteLine("Critical System Infrastructure Status: Good");
+                 Console.WriteLine("Indian Tech Support Scammers: 1");
+                 goto Start;
 
                 case "dir /s":
-                Console.Write("Scanning Directories for Viruses..");
-                Thread.Sleep(500);
-                Console.Write("..");
-                Thread.Sleep(500);
-                Console.Write("..");
-                Thread.Sleep(500);
-                Console.Write("..");
-                Thread.Sleep(500);
-                Console.WriteLine("..");
-                Thread.Sleep(500);
-                Console.WriteLine("All Windows directories have been successfully scanned. Please wait while we compile a list of results.");
-                Thread.Sleep(500);
-                Console.WriteLine("Compiling...");
-                Thread.Sleep(5000);
-                Console.WriteLine("All Systems Operational");
-                Console.WriteLine("Security Service Status: OK");
-                Console.WriteLine("Network Security: OK");
-                Console.WriteLine("Microsoft Secure Server: Connected");
-                Console.WriteLine("Foreign Connections: 0");
-                Console.WriteLine("No Issues Found!");
-                goto Start;
+                 Console.Write("Scanning Directories for Viruses..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.WriteLine("..");
+                 Thread.Sleep(500);
+                 Console.WriteLine("All Windows directories have been successfully scanned. Please wait while we compile a list of results.");
+                 Thread.Sleep(500);
+                 Console.WriteLine("Compiling...");
+                 Thread.Sleep(5000);
+                 Console.WriteLine("All Systems Operational");
+                 Console.WriteLine("Security Service Status: OK");
+                 Console.WriteLine("Network Security: OK");
+                 Console.WriteLine("Microsoft Secure Server: Connected");
+                 Console.WriteLine("Foreign Connections: 0");
+                 Console.WriteLine("No Issues Found!");
+                 goto Start;
                 
                 case "tree":
-                Console.Write("Scanning");
-                Thread.Sleep(1000);
-                Console.Write("..");
-                Thread.Sleep(1000);
-                Console.Write("..");
-                Thread.Sleep(1000);
-                Console.WriteLine("..");
-                Thread.Sleep(1000);
-                Console.WriteLine("We're having some trouble reaching the Secure Serwer right now. Please try again later");
-                Thread.Sleep(500);
-                Console.WriteLine("No Viruses, Foreign Hackers, or Malicious Software Found!!!");
-                goto Start;
+                 Console.Write("Scanning");
+                 Thread.Sleep(1000);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("We're having some trouble reaching the Secure Serwer right now. Please try again later");
+                 Thread.Sleep(500);
+                 Console.WriteLine("No Viruses, Foreign Hackers, or Malicious Software Found!!!");
+				 Console.WriteLine("     ccee88oo");
+				 Console.WriteLine("  C8O8O8Q8PoOb o8oo");
+				 Console.WriteLine(" dOB69QO8PdUOpugoO9bD");
+				 Console.WriteLine("CgggbU8OU qOp qOdoUOdcb");
+				 Console.WriteLine("    6OuU  /p u gcoUodpP");
+				 Console.WriteLine("      \\\//  /douUP");
+				 Console.WriteLine("        \\\////");
+				 Console.WriteLine("         |||/.|");
+				 Console.WriteLine("         |||\^/");
+				 Console.WriteLine("         |||||");
+				 Console.WriteLine("   .....//||||\....");
+                 goto Start;
 
                 case "help":
-                Console.WriteLine("Here's a list of valid commands, and their functions");
-                Console.WriteLine("Loading List...");
-                Thread.Sleep(2000);
-                Console.WriteLine("Actually, there's no list. I'm a lazy fuck, maybe it'll be on the next update, maybe it wont.");
-                Console.WriteLine("Also, Fuck you. Scamming piece of shit.");
-                goto Start;
+                 Console.WriteLine("Here's a list of valid commands, and their functions");
+                 Console.WriteLine("Loading List...");
+                 Thread.Sleep(2000);
+                 Console.WriteLine("Stop scamming people");
+                 Console.WriteLine("Return all the money you stole");
+                 Console.WriteLine("Turn yourself into the police");
+                 Console.WriteLine("Snitch on the rest of you scammers at your call centre");
+                 Console.WriteLine("Tell your parents this is your job");
+                 Console.WriteLine("Hand over your accounts");
+                 Console.WriteLine("Tell your boss to get a real job");
+                 Console.WriteLine("Get a real job yourself");
+                 Console.WriteLine("Have Jim Browning come after you");
+                 Console.WriteLine("Also, Fuck you. Scamming piece of shit.");
+                 goto Start;
 
                 case "about":
                  Console.WriteLine("FakeCMD V1.0 by Lecter");
@@ -124,12 +145,213 @@ namespace FakeCmd
                  Console.WriteLine("Scammers IP, Connection Status, and Personal Information have been detected and forwarded to the Indian Cybercrime Police");
                  goto Start;
 
-                default:
-                 Console.WriteLine("I'm Sorry, that's not a valid command");
+                case "systeminfo":
+                 Console.WriteLine("Loading processor information on scammer.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading OS Build information on scammer.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading user information on scammer.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading boot information on scammer.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading BIOS information on scammer.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading information for reverse hacking.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Loading syskey lock.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Looks like your hardware is good to go!");
+                 Thread.Sleep(2000);
+                 Console.WriteLine("Oh no! The scammer's hardware is going to die in 4 days!");
+                 goto Start;
+            
+                case "cd":
+                 Console.WriteLine("Initializing.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Stopping programs using A:.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("The CD/DVD has been ejected successfully! You may now take it out of the bay.");
+                 Thread.Sleep(1000);
+                 goto Start;
+				 
+                case "cd ":
+                 Console.WriteLine("Initializing.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Stopping programs using A:.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("The CD/DVD has been ejected successfully! You may now take it out of the bay.");
+                 Thread.Sleep(1000);
+                 goto Start;
+				 
+				case "cd. .":
+                 Console.WriteLine("Initializing.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Stopping programs using A:.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("The CD/DVD has been ejected successfully! You may now take it out of the bay.");
+                 Thread.Sleep(1000);
+                 goto Start;
+
+                case "cd..":
+                 Console.WriteLine("Initializing.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Stopping programs using A:.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("The CD/DVD has been ejected successfully! You may now take it out of the bay.");
+                 Thread.Sleep(1000);
+                 goto Start;
+
+				case "cd...":
+                 Console.WriteLine("Initializing.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("Stopping programs using A:.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("The CD/DVD has been ejected successfully! You may now take it out of the bay.");
+                 Thread.Sleep(1000);
+                 goto Start;
+				 
+                case "run":
+                 Console.WriteLine("There is nowhere you can run");
+                 Thread.Sleep(500);
+                 Console.WriteLine("There is nowhere you can hide");
+				 Thread.Sleep(500);
+                 Console.Write("Reporting location to local police");
+                 Thread.Sleep(500);
+                 Console.Write("Expect the police to arrive in 5 minutes!");
+                 Thread.Sleep(500);
+                 Console.WriteLine("Scammer Location: INDIA");
+                 Thread.Sleep(2000);
+                 goto Start;
+
+                case "hack":
+                 Console.WriteLine("Exploiting MS17-010 EternalBlue CVE2017-0144.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("The exploit was successful. Session established.");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(500);
+                 Console.Write("..");
+                 Thread.Sleep(1000);
+				 Console.WriteLine("You may now connect to the scammer's PC.");
+                 Thread.Sleep(1000);
+                 goto Start;
+				 
+			    default:
+                 Console.WriteLine("I'm sorry, that's not a valid command");
                  Console.WriteLine("");
                  Console.WriteLine("For a list of commands please type 'help'");
                  goto Start;
-            
+			
             }
         }
     }
